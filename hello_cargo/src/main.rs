b58b143729e3cc79eaa6c7300dcc3f4379e904e9
Pedro Parrilla Bascón <pparrilla10@gmail.com>
2025-02-17 02:24:31 +0100

    collatz lenght implementation

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,12 +1,48 @@
+fn main() {
+    let n: u32 = 5;
+    println!("⚙️ ~ fnmain ~ n: {}", n);
+    println!("fib({n}): {}" , fib(n));
+
+    println!("{n}! = {}", factorial(n));
+    //printing a todo!()
+    // fizzbuzz(n);
+    
+    println!("Lenght: {}", collatz_lenght(11)) ;
+}
+
 fn fib(n: u32) -> u32 {
     if n < 2{
-        return n;
+        n
     } else { 
-        return fib(n-1) + fib(n-2);
+        fib(n-1) + fib(n-2)
     }
 }
 
-fn main() {
-    let n = 5;
-    println!("fib({n}): {}" , fib(n));
+fn factorial(n: u32) -> u32 {
+    let mut product = 1;
+    for i in 1..=n { 
+        product *= dbg!(i);
+    }
+    product
 }
+
+fn fizzbuzz (_n: u32) {
+    todo!();
+}
+
+
+//Determine the length of the collatz sequence beginning at 'n'
+fn collatz_lenght(mut n: i32) -> i32 {
+    let mut iter = 1;
+    while n > 1 {
+        n = if n%2 == 0 { n / 2 } else { 3 * n + 1};
+        iter += 1;
+    }
+    iter
+}
+
+#[test]
+fn test_collatz_lenght() {
+    assert_eq!(collatz_lenght(11), 15);
+}
+
---
+++ hello_cargo/target/debug/deps/hello_cargo-c365baaf71dfeafc.d
@@ -0,0 +1,5 @@
+E:\Projects\rust_learning\hello_cargo\target\debug\deps\hello_cargo-c365baaf71dfeafc.exe: src\main.rs
+
+E:\Projects\rust_learning\hello_cargo\target\debug\deps\hello_cargo-c365baaf71dfeafc.d: src\main.rs
+
+src\main.rs:
